3af0da13647a95e789163e7a9700b823f0effce9
alexanderjson <alexanderjson@hotmail.com>
2026-02-16 03:26:42 +0100

    added jest npm pck for cover test

---
+++ tests/storage.test.js
@@ -0,0 +1,17 @@
+import {
+  TASK_STATUSES,
+  isValidTaskStatus,
+  requiresCommentForStatus
+} from "../js/status.js";
+
+/*
+
+  tester:
+  loadState: ska return json data med key, ska skicka ut 0 tasks och 2 prs vid ingen data
+  save: ska kunna lägga till
+  
+
+
+
+
+*/
